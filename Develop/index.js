7da924e5d60ddee3ba76d5cc7869fcae2ec4d927
Annie Hoang <anniehoang.anh@gmail.com>
2022-07-29 19:26:58 -0400

    wrote function to trasnfer user input to README template

--- Develop/index.js
+++ Develop/index.js
@@ -1,64 +1,71 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const generateMarkdown= require("./utils/generateMarkdown")
-const fs=require('fs') 
+const generateMarkdown = require("./utils/generateMarkdown")
+const fs = require('fs')
 // TODO: Create an array of questions for user input
 // Description, Table of Contents, Installation, Usage, License, Contributing, Tests, and Questions
 const questions = [
     {
-        type:"input",
-        name:"title",
-        message:"What is the title of your project?"
+        type: "input",
+        name: "title",
+        message: "What is the title of your project?"
     },
     {
-        type:"input",
-        name:"description",
-        message:"Provide a short description explaining the what, why, and how of your project:"
+        type: "input",
+        name: "description",
+        message: "Provide a short description explaining the what, why, and how of your project:"
     },
     {
-        type:"input",
-        name:"toc",
-        message:"Add a table of contents:"
+        type: "input",
+        name: "installation",
+        message: "What are the steps required to install your project?"
     },
     {
-        type:"input",
-        name:"installation",
-        message:"What are the steps required to install your project?"
+        type: "input",
+        name: "usage",
+        message: "Provide instructions and examples for use:"
     },
     {
-        type:"input",
-        name:"usage",
-        message:"Provide instructions and examples for use:"
+        type: "list",
+        name: "license",
+        message: "Choose a license:",
+        choices: ["Apache", "IBM", "MIT", "Mozilla", "The Unlicense", "none"]
     },
     {
-        type:"list",
-        name:"license",
-        message:"Choose a license:",
-        choices:["GNU AGPLv3","GNU GPLv3","GNU LGPLv3","Mozilla","Apache","MIT","Boost Software","Other"]
+        type: "input",
+        name: "contributing",
+        message: "Include guidelines for how others can contribute to your work:"
     },
     {
-        type:"input",
-        name:"contributing",
-        message:"Include guidelines for how others can contribute to your work:"
+        type: "input",
+        name: "tests",
+        message: "Write tests for your application then provide examples on how to run them:"
     },
     {
-        type:"input",
-        name:"tests",
-        message:"Write tests for your application then provide examples on how to run them:"
+        type: "input",
+        name: "questions",
+        message: "What is your GitHub username?"
     },
     {
-        type:"input",
-        name:"questions",
-        message:"Questions concerning your project:"
+        type: "input",
+        name: "email",
+        message: "What is your email?"
     },
-];
 ];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+
+    inquirer.prompt(questions)
+        .then(data => {
+            console.log(data)
+            fs.writeFileSync("./output/README.md", generateMarkdown(data))
+        })
+
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,82 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) { 
+  if(license==="Apache"){
+    return '[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)'
+  }
+  else if(license==="IBM"){
+    return '[![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)](https://opensource.org/licenses/IPL-1.0)'
+  }
+  else if(license==="MIT"){
+    return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)'
+  }
+  else if(license==="Mozilla"){
+    return '[![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)](https://opensource.org/licenses/MPL-2.0)'
+  }
+  else if(license==="The Unlicense"){
+    return '[![License: Unlicense](https://img.shields.io/badge/license-Unlicense-blue.svg)](http://unlicense.org/)'
+  }
+  else if(license==="none"){
+    return ""
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) { }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) { }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+
+  ## Description
+  
+  ${data.description}
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  ${data.installation}
+
+  ## Usage
+  
+  ${data.usage}
+  
+  To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+  
+      md
+      ![alt text](assets/images/screenshot.png)
+  
+  ## License
+  
+  ${renderLicenseBadge(data.license)}
+  
+
+  ## Contributing
+  
+  ${data.contributing}
+
+  ## Tests
+  
+  ${data.tests}
+
+  ## Questions
+
+  https://github.com/${data.questions}
+
+  Please email me at ${data.email} for any additional questions.
 
 `;
 }
